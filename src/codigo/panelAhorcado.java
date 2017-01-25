ad3b0ca860f67cdbcee94bcae8d06844b5a858d0
xp <xp@DESKTOP-KF4UIC2>
2017-01-25 12:59:56 +0100

    En esta version el programa cambia las letras de la pantalla con  un StringBuilder

---
+++ src/codigo/panelAhorcado.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package codigo;
+
+/**
+ *
+ * @author xp
+ */
+class panelAhorcado {
+
+    static int getWidth() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
